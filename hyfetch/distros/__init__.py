905cb8dd4200a567c7ea144b2dfdea8fc0f266da
Luna <contact@luna.computer>
2024-05-30 19:59:06 -0700

    Support backwards compatible segmentation of existing distro ascii art

--- hyfetch/distros/__init__.py
+++ hyfetch/distros/__init__.py
@@ -53,3 +53,21 @@ class AsciiArt:
 
     def slots(self) -> set[int]:
         return set(map(int, re.findall('(?<=\\${c)[0-9](?=})', self.ascii)))
+
+    def extrapolate_missing_custom_colors(self, custom_colors: dict[int, int]) -> dict[int, int]:
+        """
+        When an AsciiArt is improved for custom coloring by adding more slots, its appearance
+        does does not change. Thus, the new slots all must have original colors (AsciiArt color field)
+        copied from the original color list. If we have a custom_colors value from hyfetch.json
+        originating before such an AsciiArt improvement, we can extrapolate for a missing slot by
+        finding that slot's original color, and using the same custom color as the first slot with
+        the same original color. This is a fully correct mapping if the original AsciiArt had unique
+        original colors across all *used* slots.
+
+        :param custom_colors: Map of slot (e.g. ${c1}) indices to unique color indices
+        :return: Copy of custom_colors with additional entries covering missing slots
+        """
+        colors = self.color.split(' ')
+        backup_slot_map = {i: colors.index(color) + 1 for i, color in enumerate(colors, 1)}
+        redirect_slot = lambda slot: slot if slot in custom_colors else backup_slot_map[slot]
+        return {slot: custom_colors[redirect_slot(slot)] for slot in self.slots()}
